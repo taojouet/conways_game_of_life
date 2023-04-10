8217474ce32b444508beb2b69ffef8f2c0b2033d
taojouet <taojouet@gmail.com>
2023-04-10 23:01:55 +0200

    Add checkerboard v1

--- main.py
+++ main.py
@@ -2,17 +2,35 @@ import pygame
 import numpy as np
 
 
+color1 = (255, 0, 0)
+color2 = (0, 255, 0)
+
+colors = [color1, color2]
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
 running = True
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    # pygame.time.wait(2000)
+
+    screen.fill("gray")
+
+    for x in range(0, 1280, 90):
+        pygame.draw.line(screen, "black", [x, 0], [x, 720], width=1)
+
+    for y in range(0, 720, 90):
+        pygame.draw.line(screen, "black", [0, y], [1280, y], width=1)
+    
     pygame.display.flip()
+    
+    clock.tick(60)  # limits FPS to 60
 
 
 pygame.quit()
